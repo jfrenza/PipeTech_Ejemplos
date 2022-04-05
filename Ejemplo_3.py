57abd8227cfbe04896a88a1fb33342a13cda8851
jfrenza <jfrenzac@gmail.com>
2022-04-05 15:59:28 -0500

    Ejemplos

---
+++ Ejemplo_3.py
@@ -0,0 +1,16 @@
+'''Divisible By Ten
+
+Let’s start our code challenges with
+a function that counts how many numbers
+are divisible by ten from a list of numbers.
+This function will accept a list of numbers as
+an input parameter and use a loop to check each of the numbers
+in the list. Every time a number is divisible by 10,
+a counter will be incremented and the final
+count will be returned. These are the steps we need to complete this:
+
+Define the function to accept one input parameter called nums
+Initialize a counter to 0
+Loop through every number in nums
+Within the loop, if any of the numbers are divisible by 10, increment our counter
+Return the final counter value'''
---
+++ Ejemplos_1.py
@@ -0,0 +1,13 @@
+'''
+Large Power
+
+For the first code challenge,
+we are going to create a method that tests whether
+the result of taking the power of one number
+to another number provides an answer which is greater than 5000.
+We will use a conditional statement to return True if the result is greater than 5000
+or return False if it is not. In order to accomplish this, we will need the following steps:
+
+Define the function to accept two input parameters called base and exponent
+Calculate the result of base to the power of exponent
+Use an if statement to test if the result is greater than 5000. If it is then return True. Otherwise, return False'''
